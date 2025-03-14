5aeb9a85ae861e91e330c4a1c5d0daadfb3b2618
Kartik Sharma <Kartik.cityu@gmail.com>
2025-03-14 16:37:52 +0800

    update

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -50,7 +50,7 @@ const about = {
 }
 const skills = {
   title: 'My Skills',
-  description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium minus ut nesciunt unde, saepe doloribus fugiat facere molestias, quo in ab laborum incidunt atque quas omnis dolorum cumque iure cum.",
+  description: "M",
   skillList: [
     {
       icon:<FaHtml5/>,
@@ -177,8 +177,8 @@ const Resume = () => {
 
                 <TabsContent value = "education" className = "w-full">
                   <div className="flex flex-col gap-[30px] text-center xl:text-left">
-                        <h3 className="text-4xl font-bold">{experience.title}</h3>
-                        <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{experience.description}</p>
+                        <h3 className="text-4xl font-bold">{education.title}</h3>
+                        <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{education.description}</p>
                     
                         <ScrollArea className="h-[400px]">
                             <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
