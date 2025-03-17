5d2b2df4d6275e0c71c7e13f2228bf1fd46e2846
Kartik Sharma <Kartik.cityu@gmail.com>
2025-03-17 22:18:27 +0800

    update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,6 +1,50 @@
+"use client";
+
+import {Button} from "@/components/button";
+import {Input} from  "@/components/ui/input";
+import {Textarea} from "@/components/Textarea";
+
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+
+import {FaPhoneAlt, FaEnvelope, FaMapMarkerAlt} from "react-icons/fa";
+
+const info = [
+    {
+        icon: <FaPhoneAlt/>,
+        title: "Phone",
+        description: "(+852) 5634 5890"
+    },
+    {
+        icon: <FaEnvelope/>,
+        title: "Email",
+        description: "kartikrec28@gmail.com"
+    },
+    {
+        icon: <FaMapMarkerAlt/>,
+        title: "Address",
+        description: "1001, Hall 14, Student Residence, City University of Hong Kong"
+    }
+];
+
+import {motion} from "framer-motion";
+
 const Contact = () => {
     return (
-        <div>contact page</div>
+        <motion.section 
+        intial = {{opacity:0}}
+        animate = {{opacity:1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn"}}}
+        >
+            
+        </motion.section>
     )
 }
 
--- app/work/page.jsx
+++ app/work/page.jsx
@@ -18,11 +18,150 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 
+const projects = [
+    {
+        num: '01',
+        category: 'fullstack',
+        title: 'HandyHive',
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ipsam quis eum sed quam sapiente eveniet aut rerum, exercitationem consequuntur et, recusandae distinctio esse! Provident eveniet eligendi ad nam id.",
+        stack: [
+            {name: "ReactJs" },
+            {name: "NodeJs" },
+            {name: "MySQL"}
+        ],
+        image: '/assets/work/thumb1.png',
+        live: '',
+        github: ''
+    },
+
+    {
+        num: '02',
+        category: 'fullstack',
+        title: "Don't Throw Me (DTM)",
+        description: "A platform to make it easier for you to buy, sell, swap, donate, and much more.",
+        stack: [
+            {name: "VueJs" },
+            {name: "PHP" },
+            {name: "CodeIgnitor"},
+            {name: "MySQL"}
+        ],
+        image: '/assets/work/thumb1.png',
+        live: 'https://dontthrowme.com/en/about-us',
+        github: ''
+    },
+
+    {
+        num: '03',
+        category: 'fullstack',
+        title: 'Karven - Ultimate Banking Solution',
+        description: "Built with Next.js, Karven is a financial SaaS platform that connects to multiple bank accounts, displays transactions in real-time, allows users to transfer money to other platform users, and manages their finances altogether.",
+        stack: [
+            {name: "Next.js" },
+            {name: "TypeScript" },
+            {name: "Appwrite"},
+            {name: "Plaid" },
+            {name: "Dwolla" },
+            {name: "React Hook Form"},
+            {name: "Zod" },
+            {name: "TailwindCSS" },
+            {name: "Chart.js"},
+            {name: "ShadCN"}
+        ],
+        image: '/assets/work/thumb1.png',
+        live: '',
+        github: '',
+    },
+];
 
 const Work = () => {
-    return (
-        <div>work page</div>
-    )
+   const [project, setProject] = useState(projects[0]);
+
+   const handleSlideChange = (swiper) => {
+        const currentIndex = swiper.activeIndex;
+
+        setProject(projects[currentIndex]);
+   };
+
+   return (
+        <motion.section 
+            intial = {{opacity:0}}
+            animate = {{opacity:1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn"}}}
+            className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+        >
+            <div className="container mx-auto">
+                <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+                    <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none ">
+                        <div className="flex flex-col gap-[30px] h-[50%]">
+                            {/* outline num */}
+                            <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                                {project.num}
+                            </div>
+                            {/* project category */}
+                            <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                                {project.category} project
+                            </h2>
+                            {/* project description */}
+                            <p className="text-white/60">{project.description}</p>
+                             {/* stack */}
+                             <ul className="flex gap-4">
+                                {project.stack.map((item, index) =>{
+                                    return (
+                                <li key = {index} className="text-xl text-accent">
+                                    {item.name}
+                                    {index !== project.stack.length - 1 && ","}
+                                </li>
+                                );
+                                })}
+                            </ul>
+                            {/* Border */}
+                            <div className = "border border-white/20"></div>
+                            <div className="flex items-center gap-4">
+                                <Link href={project.live}>
+                                    <TooltipProvider delayDuration = {100}>
+                                        <Tooltip>
+                                            <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center item-center group">
+                                                <BsArrowUpRight className="text-white text-3xl group-hover:text-accent"/>
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                                <p>Live project</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                </Link>
+                                <Link href={project.github}>
+                                    <TooltipProvider delayDuration = {100}>
+                                        <Tooltip>
+                                            <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center item-center group">
+                                                <BsGithub className="text-white text-3xl group-hover:text-accent"/>
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                                <p>Github repository</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                </Link>
+                            </div>    
+                        </div> 
+                    </div>
+                    <div className="w-full xl:w-[50%]">
+                        <Swiper spaceBetween={30} slidesPerView={1} className="xl:h-[520px] mb-12" onSlideChange={handleSlideChange}>
+                            {projects.map((project,index)=> {
+                                return <SwiperSlide key = {index} className="w-full">
+                                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                                        <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                                        <div className="relative w-full h-full">
+                                            <Image src={project.image} fill className="object-cover" alt=""/>
+                                        </div>
+                                    </div>
+                                </SwiperSlide>;
+                            })}
+                            <WorkSliderBtns></WorkSliderBtns>
+                        </Swiper>
+                    </div>
+                </div>
+            </div>
+        </motion.section>
+   )
 }
 
 export default Work;
